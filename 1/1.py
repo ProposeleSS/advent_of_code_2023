a625ed86048f3cc8c3a71d9de348eb5d33f06eaa
Domantas <weselis@gmail.com>
2023-12-05 10:37:07 +0200

    day 1 part 2 still got issues

--- 1/1.py
+++ 1/1.py
@@ -1,4 +1,5 @@
 from string import digits
+import re
 import pdb
 
 with open('1/input.txt') as f:
@@ -24,18 +25,20 @@ for line in input:
     tmp_line = line
     idxs = {}
     for digit in digits_spelled.keys():
-        idx = tmp_line.find(digit)
-        if idx != -1:
+        # idx = tmp_line.find(digit)
+        idx = [m.start() for m in re.finditer(digit, line)]
+        if len(idx) > 0:
             idxs[digit] = idx
     idxs_sorted = sorted(idxs.items(), key=lambda x: x[1])
     if len(idxs_sorted) > 0:
-        print(line.strip())
-        print(idxs)
-        print()
         first = idxs_sorted[0]
         last = idxs_sorted[-1]
+        # if len(first[1]) > 1:
+        #     first[1] = [first[1][0]]
+        # if len(last[1]) > 1:
+        #     last[1] = [last[1][-1]]
         tmp_line = tmp_line.replace(first[0], str(digits_spelled[first[0]]))
-        tmp_line = tmp_line.replace(last[0], str(digits_spelled[last[0]]))
+        tmp_line = str(digits_spelled[last[0]]).join(tmp_line.rsplit(last[0], 1))
 
     input2.append(tmp_line) 
 
@@ -44,3 +47,4 @@ print('part 2:')
 print(solve(input2))
 
 # kazkodel part2 atsakymo neprieme, aceit klaida
+ 
